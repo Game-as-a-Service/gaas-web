cc5e573a8d7dabb4d7fd6f84c705bd9bac57c42e
wally5077 <wally55077@gmail.com>
2021-09-02 17:47:35 +0800

    create basic Dixit fragment

---
+++ src/app/dixit/Dixit.tsx
@@ -0,0 +1,12 @@
+import './Dixit.css';
+import PlayerBar from "./playerbar/PlayerBar";
+
+const Dixit = () => {
+    return (
+        <div className="dixit">
+            <PlayerBar/>
+        </div>
+    );
+}
+
+export default Dixit;
---
+++ src/app/dixit/logo/Logo.tsx
@@ -0,0 +1,14 @@
+import './Logo.scss';
+
+const Logo = () => {
+    return (
+        <div className="dixit-logo">
+            <span className="indigo">D</span>
+            <span className="violet">I</span>
+            <span className="red">X</span>
+            <span className="yellow">I</span>
+            <span className="indigo">T</span>
+        </div>
+    );
+}
+export default Logo;
